9a19442491de58b9c17d72e67c94653ecdf25142
Jesse Wright <31148146+cisphon@users.noreply.github.com>
2020-09-10 17:41:12 -0500

    Did a test.

--- MATH2318/MATH2318.cpp
+++ MATH2318/MATH2318.cpp
@@ -805,8 +805,98 @@ namespace MATH2318 {
             std::cout << Tools::pow_of_matrix(B, 1 / n) << std::endl;
         }
     }
+
+    namespace TEST1 {
+        void question1() {
+
+        }
+
+        void question3() {
+            Eigen::MatrixXf A(4, 3);
+            A << 3, 3, 12,
+                1, 1, 4,
+                2, 5, 20,
+                -1, 2, 8;
+
+            Eigen::VectorXf V(A.rows());
+            V << 6, 2, 10, 4;
+
+            std::cout << A.fullPivHouseholderQr().solve(V);
+        }
+
+        void question6() {
+            Eigen::MatrixXf A(3, 2);
+            A << 2, 1,
+                -4, 3,
+                1, 6;
+
+            Eigen::MatrixXf B(3, 3);
+            B << 0, -1, 0,
+                3, 0, 1,
+                8, -1, 6;
+
+            //std::cout << A * B << std::endl;
+
+
+            std::cout << B * A << std::endl;
+        }
+
+        void question8() {
+            Eigen::MatrixXf A(2, 3);
+            A << 2, 1, 1,
+                -1, 3, 0;
+
+            Eigen::MatrixXf B(2, 2);
+            B << 3, 2,
+                -1, 1;
+
+            Eigen::MatrixXf C(2, 2);
+            C << 1, 0,
+                0, -1;
+
+            std::cout << (B + C) * A << std::endl;
+        }
+
+        void question9() {
+            Eigen::MatrixXf A(2, 3);
+            A << 2, -1, 2,
+                1, 4, 0;
+
+            Eigen::MatrixXf a = A.transpose() * A;
+            std::cout << a << std::endl << "~~~~~~~~~~" << std::endl;
+
+            Eigen::MatrixXf b = A * A.transpose();
+            std::cout << b << std::endl;
+
+            std::cout << Tools::isSymmetric(a) << std::endl;
+            std::cout << Tools::isSymmetric(b) << std::endl;
+        }
+
+        void question11() {
+            Eigen::MatrixXf A(4, 4);
+
+            A << -18, 0, 0, 0,
+                0, 14, 0, 0,
+                0, 0, 0, 0,
+                0, 0, 0, 17;
+
+            std::cout << A.inverse() << std::endl;
+        }
+
+        void question14() {
+            Eigen::MatrixXf L(3, 3);
+            L << 1, 0, 0,
+                0, 1, 0,
+                -1, 2, 1;
+
+            Eigen::VectorXf y(3);
+            y << 4, 8, -8;
+
+            std::cout << L * y << std::endl;
+        }
+    }
 }
 
 int main() {
-    MATH2318::QUIZ2::question4();
+    MATH2318::TEST1::question3();
 }
